05de2b85395beafacecd509c1fec9b8a7222259c
e <157100278364@163.com>
2017-07-31 10:23:51 +0800

    logs

---
+++ app/src/main/java/zhixiang/com/news_mvp/MainActivity.java
@@ -0,0 +1,13 @@
+package zhixiang.com.news_mvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
